69ee93021e6f49aefce18b496e128c6b6433887c
AmosG <amosg86@gmail.com>
2024-11-05 19:23:22 +0800

    further fixes

--- pages/2_Methodology.py
+++ pages/2_Methodology.py
@@ -4,6 +4,13 @@ import os
 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+from logics.check_password import check_password
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
 st.set_page_config(page_title="Methodology", page_icon="👨")
 contentfile = "content/Method.xml"
 
